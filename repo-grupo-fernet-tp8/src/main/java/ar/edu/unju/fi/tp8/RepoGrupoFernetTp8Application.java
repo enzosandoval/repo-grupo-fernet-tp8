193b8330cac60f16efe72464676aa37e6d2466e2
Enzo Aramayo <enzosandoval@hotmail.com>
2021-05-30 10:50:21 -0300

    Base

---
+++ repo-grupo-fernet-tp8/src/main/java/ar/edu/unju/fi/tp8/RepoGrupoFernetTp8Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoGrupoFernetTp8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoGrupoFernetTp8Application.class, args);
+	}
+
+}
